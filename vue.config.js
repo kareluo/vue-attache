662c829af441d2c30fb0a9a80459764b3c3db744
Felix <kareluo76@gmail.com>
2019-03-28 00:22:41 +0800

    vue config js

--- vue.config.js
+++ vue.config.js
@@ -8,35 +8,41 @@ const __build__ = '__build__'
 const PAGE = {
   title: 'Vue Attache Example',
   template: path.join(examples, 'index.html'),
-  chunks: ['chunk-vendors', 'chunk-common', 'index']
+  chunks: ['chunk-vendors', 'chunk-common']
 }
+
+const pages = fs.readdirSync(examples).reduce((pages, dir) => {
+  if (dir !== __build__) {
+    const fullPath = path.join(examples, dir)
+    const entry = path.join(fullPath, 'main.js')
+    if (fs.statSync(fullPath).isDirectory() && fs.existsSync(entry)) {
+      const page = Object.create(PAGE)
+      const template = path.join(fullPath, 'index.html')
+      if (fs.existsSync(template)) {
+        page.template = template
+      }
+      page.filename = `${dir}.html`
+      page.entry = entry
+      page.chunks.push(dir)
+      pages[dir] = page
+    }
+  }
+  return pages
+}, {
+  index: {
+    ...PAGE,
+    entry: `${examples}/main.js`,
+    chunks: ['chunk-vendors', 'chunk-common', 'index']
+  }
+})
+
+console.log(pages)
 
 module.exports = {
 
   outputDir: `${examples}/${__build__}`,
 
-  pages: fs.readdirSync(examples).reduce((pages, dir) => {
-    if (dir !== __build__) {
-      const fullPath = path.join(examples, dir)
-      const entry = path.join(fullPath, 'main.js')
-      if (fs.statSync(fullPath).isDirectory() && fs.existsSync(entry)) {
-        const page = Object.create(PAGE)
-        const template = path.join(fullPath, 'index.html')
-        if (fs.existsSync(template)) {
-          page.template = template
-        }
-        page.filename = `${dir}.html`
-        page.entry = entry
-        pages[dir] = page
-      }
-    }
-    return pages
-  }, {
-    index: {
-      ...PAGE,
-      entry: `${examples}/main.js`,
-    }
-  }),
+  pages,
 
   configureWebpack: {
     resolve: {
